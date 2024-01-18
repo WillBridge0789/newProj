cc33968624947f980432236eeb188f07442fe4c1
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2024-01-18 21:32:29 +0000

    home page still needs setup. styled by tailwind and video point-27:07

---
+++ new-proj/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">Growing with code</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with development.
+        </h1>
+        <div>
+          <p>Development in</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
