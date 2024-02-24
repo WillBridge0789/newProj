76777667d8bc4522ae7f3a1154f68af1cdbb7c22
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2024-02-24 00:01:08 +0000

    Overall landing is done. Adding a new component soon. Tutorial vid 53:30

--- new-proj/src/App.js
+++ new-proj/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Info from "./components/Info";
+import Signs from "./components/Signs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Hero />
       <Info />
+      <Signs />
     </div>
   );
 }
